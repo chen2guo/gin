2a2d687086a45af5309912657c1cf0d3474688be
chen2guo <chen2guo@foxmail.com>
2020-12-01 19:19:05 +0800

    update

--- app/app.go
+++ app/app.go
@@ -1,15 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
 
 	engine := gin.Default()
+	engine.GET("/hello", func(c *gin.Context) {
+		fmt.Println("GET URL is: ", c.FullPath())
+		c.Writer.Write([]byte("Hell, Gin.\n"))
+	})
+
 	engine.GET("/ping", func(c *gin.Context) {
+		fmt.Println("GET URL is: ", c.FullPath())
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	engine.Run()
+	if err := engine.Run("0.0.0.0:8080"); err != nil {
+		log.Fatal(err.Error())
+	}
+	//engine.Run()
 }
