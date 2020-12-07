051bc6c66d0250835688b01ff84f35fdc10214a2
Chen Bin <chen2guo@foxmail.com>
2020-12-07 16:04:50 +0800

    add

--- route/app.go
+++ route/app.go
@@ -5,34 +5,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func hello(context *gin.Context) {
+	fmt.Println(context.FullPath())
+
+	name := context.Query("name")
+	fmt.Printf("Username is %s.\n", name)
+
+	context.Writer.Write([]byte("hello, " + name + "."))
+}
+
+func login(context *gin.Context) {
+	fmt.Println(context.FullPath())
+
+	name := context.Query("name")
+	pass := context.Query("pass")
+	fmt.Printf("Username is %s; Pass is %s.\n", name, pass)
+
+	context.Writer.Write([]byte("hello, " + name + "."))
+}
+
+func user(context *gin.Context) {
+	userID := context.Param("id")
+	fmt.Println(userID)
+	context.Writer.Write([]byte("Delete Id: " + userID + "."))
+}
+
 func main() {
-
 	engine := gin.Default()
-
-	engine.GET("/hello", func(context *gin.Context) {
-		fmt.Println(context.FullPath())
-
-		name := context.Query("name")
-		fmt.Printf("Username is %s.\n", name)
-
-		context.Writer.Write([]byte("hello, " + name + "."))
-	})
-
-	engine.POST("/login", func(context *gin.Context) {
-		fmt.Println(context.FullPath())
-
-		name := context.Query("name")
-		pass := context.Query("pass")
-		fmt.Printf("Username is %s; Pass is %s.\n", name, pass)
-
-		context.Writer.Write([]byte("hello, " + name + "."))
-	})
-
-	engine.DELETE("/user/:id", func(context *gin.Context) {
-		userID := context.Param("id")
-		fmt.Println(userID)
-		context.Writer.Write([]byte("Delete Id: " + userID + "."))
-	})
-
+	engine.GET("/hello", hello)
+	engine.POST("/login", login)
+	engine.DELETE("/user/:id", user)
 	engine.Run()
 }
